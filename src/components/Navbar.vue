f36f0803db176287e96637e494a5aa81059e9806
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-02-09 15:41:37 -0300

    feat: create a navbar component, add bootstrap and create routes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-danger">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+        <img src="../assets/logo.png" alt="Pokemon" height="40" width="120" />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link text-white" aria-current="page" to="/"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link text-white" to="/about"
+              >About</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="main">
+    <router-view></router-view>
+  </div>
+</template>
