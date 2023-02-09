cf9f708a9cfcf4656982b0b66e4eeaf867d2e559
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-02-09 19:19:11 -0300

    feat: add searchbar

--- src/View/Home.vue
+++ src/View/Home.vue
@@ -1,12 +1,27 @@
 <script setup>
-import { onMounted, reactive, ref } from "vue";
+import { onMounted, reactive, ref, computed } from "vue";
+import PokemonsList from "../components/PokemonsList.vue";
 
+let baseUrlSvg = ref(
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"
+);
 let pokemons = reactive(ref());
+let searchPokemonField = ref("");
 
 onMounted(() => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
     .then((res) => res.json())
     .then((res) => (pokemons.value = res.results));
+});
+
+const pokemonsFiltered = computed(() => {
+  if (pokemons.value && searchPokemonField.value) {
+    return pokemons.value.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchPokemonField.value.toLowerCase())
+    );
+  }
+
+  return pokemons.value;
 });
 </script>
 
@@ -14,9 +29,8 @@ onMounted(() => {
   <main>
     <div class="container text-center">
       <div class="row mt-5">
-        <div class="col-sm-12 cold-md-6">
-          <div class="card" style="width: 18rem">
-            <img src="..." class="card-img-top" alt="..." />
+        <!-- <div class="card" style="width: 18rem">
+            <img src="" class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">
@@ -25,16 +39,29 @@ onMounted(() => {
               </p>
               <a href="#" class="btn btn-primary">Detalhes</a>
             </div>
-          </div>
+          </div> -->
+        <div class="mb-3">
+          <label hidden for="searchPokemonField" class="form-label"
+            >Pesquisar</label
+          >
+          <input
+            v-model="searchPokemonField"
+            type="text"
+            class="form-control"
+            id="searchPokemonField"
+            placeholder="Pesquisar..."
+          />
         </div>
 
-        <div class="col-sm-12 cold-md-6">Column</div>
-        <div class="card" style="width: 18rem">
-          <ul>
-            <li v-for="pokemon in pokemons" :key="pokemon.name">
-              {{ pokemon.name }}
-            </li>
-          </ul>
+        <PokemonsList
+          v-for="pokemon in pokemonsFiltered"
+          :key="pokemon.name"
+          :name="pokemon.name"
+          :baseUrlSvg="baseUrlSvg + pokemon.url.split('/')[6] + '.svg'"
+        />
+
+        <div class="card">
+          <div class="card-body-row"></div>
         </div>
       </div>
     </div>
---
+++ src/components/PokemonsList.vue
@@ -0,0 +1,28 @@
+<script setup>
+const pokemon = defineProps(["name", "baseUrlSvg"]);
+</script>
+
+<template>
+  <div class="col-4">
+    <div class="card p-2 mb-3 cardListPokemon">
+      <p class="text-center font-size:10vw">{{ pokemon.name }}</p>
+      <img :src="baseUrlSvg" class="card-img-top" alt="Pokemon" height="90" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cardListPokemon {
+  background: #e8e8e8;
+  cursor: pointer;
+}
+
+.cardListPokemon:hover {
+  background: #d5d1d1;
+}
+
+.cardListPokemon img:hover {
+  transition: 0.3s;
+  padding: 0.2rem;
+}
+</style>
